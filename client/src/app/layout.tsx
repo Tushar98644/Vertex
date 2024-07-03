3873121c86d26e70a3e0ec10e03f1d5728cd9f0a
Tushar Banik <b.toshar@iitg.ac.in>
2024-07-03 17:28:50 +0530

    web analytics

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 import Footer from "@/components/Footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <SpeedInsights/>
+      <Analytics/>
       <body className={inter.className}>{children}</body>
       <Footer />
     </html>
